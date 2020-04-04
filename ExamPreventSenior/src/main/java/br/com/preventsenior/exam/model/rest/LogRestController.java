576cb314ab4ce5a0c166c401bb45111acf32e37c
JeanDev <Jean@192.168.0.10>
2020-04-04 17:24:24 -0300

    Commit - Commit de funcionalidade

--- ExamPreventSenior/src/main/java/br/com/preventsenior/exam/model/rest/LogRestController.java
+++ ExamPreventSenior/src/main/java/br/com/preventsenior/exam/model/rest/LogRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,15 +42,22 @@ public class LogRestController {
 	}
 
 	@PostMapping
-	public Long save(Log log) {
+	public Long save(@RequestBody Log log) {
 		logService.save(log);
 		return log.getId();
 	}
 	
-	@DeleteMapping
-	public Boolean delete(Log log) {
-		logService.delete(log);
-		return Boolean.TRUE;
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable Long id) {
+		
+		Log log = logService.find(id);
+		
+		if (log != null) {
+			logService.delete(log);
+			return new ResponseEntity<Boolean>(Boolean.TRUE, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Boolean>(Boolean.FALSE, HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
